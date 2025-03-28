70d0aca22f88d3c57f8dfb03ca8e5d831a5a9749
Samuel Máximo <samuelgaraujo87@gmail.com>
2025-03-27 21:52:37 -0300

    feat: init struct error

--- config/rest_error.go
+++ config/rest_error.go
@@ -1,1 +1,17 @@
 package config
+
+type RestError struct {
+	Message string `json:"message"`
+	Err     string `json:"error"`
+	Code    int    `json:"code"`
+	Causes  causes `json:"causes"`
+}
+
+type causes struct {
+	Field   []string `json:"field"`
+	Message string   `json:"message"`
+}
+
+func NewRestError() *RestError {
+	return &RestError{}
+}
